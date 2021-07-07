36ce6953a21694390b3d86affe1bd115d52ca6ed
lee-man <minli.amoy@gmail.com>
2021-07-07 13:44:46 +0800

    update.

--- experiments/multi_pose_movenet.sh
+++ experiments/multi_pose_movenet.sh
@@ -1,5 +1,5 @@
 cd src
-python main.py multi_pose --exp_id coco_movenet --dataset coco_hp --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 50 --lr_step 40
+python main.py multi_pose --exp_id coco_movenet --dataset coco_hp --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 50 --lr_step 40 --num_workers 16 --resume
 # test
 python test.py multi_pose --exp_id coco_movenet --dataset coco_hp --arch movenet --keep_res --resume
 # flip test
