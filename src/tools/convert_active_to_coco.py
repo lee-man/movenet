c74812857dadd079e4393c16ae53b86e4640b822
lee-man <minli.amoy@gmail.com>
2021-07-08 23:09:41 +0800

    update.

--- src/tools/convert_active_to_coco.py
+++ src/tools/convert_active_to_coco.py
@@ -4,84 +4,83 @@ import os.path as osp
 import numpy as np
 import json
 
-# run this code in the 'mpii_human_pose_v1_u12_2' folder
-
-def check_empty(list,name):
-    
-    try:
-        list[name]
-    except ValueError:
-        return True
-
-    if len(list[name]) > 0:
-        return False
-    else:
-        return True
-
-
 db_type = 'train' # train, test
-annot_file = loadmat('mpii_human_pose_v1_u12_1')['RELEASE']
-save_path = '../annotations/' + db_type + '.json'
+annot_path = "active.json"
+# save_path = '../annotations/' + db_type + '.json'
 
-joint_num = 16
-img_num = len(annot_file['annolist'][0][0][0])
+print("Loading Acitve dataset...")
+with open(annot_path) as json_file:
+    active = json.load(json_file)
+'''
+MPII: 0 - r ankle, 1 - r knee, 2 - r hip, 3 - l hip, 4 - l knee, 5 - l ankle, 6 - pelvis, 7 - thorax, 8 - upper neck, 9 - head top, 10 - r wrist, 11 - r elbow, 12 - r shoulder, 13 - l shoulder, 14 - l elbow, 15 - l wrist
 
-aid = 0
+COCO_PERSON_KEYPOINT_NAMES = [
+    'nose', 0
+    'left_eye', 1
+    'right_eye', 2
+    'left_ear', 3
+    'right_ear', 4
+    'left_shoulder', 5
+    'right_shoulder', 6
+    'left_elbow', 7
+    'right_elbow', 8
+    'left_wrist', 9
+    'right_wrist', 10
+    'left_hip', 11
+    'right_hip', 12
+    'left_knee', 13
+    'right_knee', 14
+    'left_ankle', 15
+    'right_ankle' 16
+]
+'''
+joint_mapping = {'0': 16, '1': 14, '2': 12, '3': 11, '4': 13, '5': 15, '6': -1, '7': -1, '8': -1, '9': 0, '10': 10, '11': 8, '12': 6, '13': 5, '14': 7, '15': 9}
+joint_num = 15
+img_num = len(active)
+print("image size: ", img_num)
+
+aid = 100000
 coco = {'images': [], 'categories': [], 'annotations': []}
 for img_id in range(img_num):
     
-    if ((db_type == 'train' and annot_file['img_train'][0][0][0][img_id] == 1) or (db_type == 'test' and annot_file['img_train'][0][0][0][img_id] == 0)) and \
-        check_empty(annot_file['annolist'][0][0][0][img_id],'annorect') == False: #any person is annotated
+    filename = 'images/' + str(active[img_id]['image'])#filename
+    img = Image.open(osp.join('..', filename))
+    w,h = img.size
+    img_dict = {'id': aid, 'file_name': str(active[img_id]['image']), 'width': w, 'height': h}
+    coco['images'].append(img_dict)
     
-        filename = 'images/' + str(annot_file['annolist'][0][0][0][img_id]['image'][0][0][0][0]) #filename
-        img = Image.open(osp.join('..', filename))
-        w,h = img.size
-        img_dict = {'id': img_id, 'file_name': filename, 'width': w, 'height': h}
-        coco['images'].append(img_dict)
-
-        if db_type == 'test':
-            continue
-        
-        person_num = len(annot_file['annolist'][0][0][0][img_id]['annorect'][0]) #person_num
-        joint_annotated = np.zeros((person_num,joint_num))
-        for pid in range(person_num):
-            
-            if check_empty(annot_file['annolist'][0][0][0][img_id]['annorect'][0][pid],'annopoints') == False: #kps is annotated
-                
-                bbox = np.zeros((4)) # xmin, ymin, w, h
-                kps = np.zeros((joint_num,3)) # xcoord, ycoord, vis
-
-                #kps
-                annot_joint_num = len(annot_file['annolist'][0][0][0][img_id]['annorect'][0][pid]['annopoints']['point'][0][0][0])
-                for jid in range(annot_joint_num):
-                    annot_jid = annot_file['annolist'][0][0][0][img_id]['annorect'][0][pid]['annopoints']['point'][0][0][0][jid]['id'][0][0]
-                    kps[annot_jid][0] = annot_file['annolist'][0][0][0][img_id]['annorect'][0][pid]['annopoints']['point'][0][0][0][jid]['x'][0][0]
-                    kps[annot_jid][1] = annot_file['annolist'][0][0][0][img_id]['annorect'][0][pid]['annopoints']['point'][0][0][0][jid]['y'][0][0]
-                    kps[annot_jid][2] = 1
-               
-                #bbox extract from annotated kps
-                annot_kps = kps[kps[:,2]==1,:].reshape(-1,3)
-                xmin = np.min(annot_kps[:,0])
-                ymin = np.min(annot_kps[:,1])
-                xmax = np.max(annot_kps[:,0])
-                ymax = np.max(annot_kps[:,1])
-                width = xmax - xmin - 1
-                height = ymax - ymin - 1
-                
-                # corrupted bounding box
-                if width <= 0 or height <= 0:
-                    continue
-                # 20% extend    
-                else:
-                    bbox[0] = (xmin + xmax)/2. - width/2*1.2
-                    bbox[1] = (ymin + ymax)/2. - height/2*1.2
-                    bbox[2] = width*1.2
-                    bbox[3] = height*1.2
+    bbox = np.zeros((4)) # xmin, ymin, w, h
+    kps = np.zeros((joint_num, 3)) # xcoord, ycoord, vis
+    #kps
+    for jid in range(16):
+        if (joint_mapping[str(jid)] == -1): continue
+        kps[jid][0] = active["joints"][jid][0]
+        kps[jid][1] = active["joints"][jid][1]
+        kps[jid][2] = active["joint_vis"] + 1
+    kps[1:5] = np.zeros((4, 3))
 
 
-                person_dict = {'id': aid, 'image_id': img_id, 'category_id': 1, 'area': bbox[2]*bbox[3], 'bbox': bbox.tolist(), 'iscrowd': 0, 'keypoints': kps.reshape(-1).tolist(), 'num_keypoints': int(np.sum(kps[:,2]==1))}
-                coco['annotations'].append(person_dict)
-                aid += 1
+    #bbox extract from annotated kps
+    annot_kps = kps[kps[:,2]==1,:].reshape(-1,3)
+    xmin = np.min(annot_kps[:,0])
+    ymin = np.min(annot_kps[:,1])
+    xmax = np.max(annot_kps[:,0])
+    ymax = np.max(annot_kps[:,1])
+    width = xmax - xmin - 1
+    height = ymax - ymin - 1
+    
+    # corrupted bounding box
+    if width <= 0 or height <= 0:
+        continue
+    # 20% extend    
+    else:
+        bbox[0] = (xmin + xmax)/2. - width/2*1.2
+        bbox[1] = (ymin + ymax)/2. - height/2*1.2
+        bbox[2] = width*1.2
+        bbox[3] = height*1.2
+    person_dict = {'id': aid, 'image_id': aid, 'category_id': 1, 'area': bbox[2]*bbox[3],'bbox':bbox.tolist(), 'iscrowd': 0, 'keypoints': kps.reshape(-1).tolist(), 'num_keypoints':int(np.su(kps[:,2]==1))}
+    coco['annotations'].append(person_dict)
+    aid += 1
 
 category = {
     "supercategory": "person",
