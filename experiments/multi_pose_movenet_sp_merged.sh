ebd9147527b8b848327713146c8a1a486b7bbcde
lee-man <minli.amoy@gmail.com>
2021-07-24 11:49:17 +0800

    update.

--- experiments/multi_pose_movenet_sp_merged.sh
+++ experiments/multi_pose_movenet_sp_merged.sh
@@ -1,5 +1,5 @@
 cd src
-python main.py multi_pose --exp_id cocosp_merge_movenet --dataset coco_hpsp_merge --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 250 --lr_step 40 --num_workers 16 --resume
+python main.py multi_pose --exp_id cocosp_merge_movenet --dataset coco_hpsp_merge --dataset coco_hpsp --arch movenet --batch_size 64 --master_batch 4 --lr 5e-4 --gpus 0,1,2,3 --num_epochs 320 --lr_step 270,300 --num_workers 16 --load_model ../models/ctdet_movenet.pth
 # test
 python test.py multi_pose --exp_id cocosp_merge_movenet --dataset coco_hpsp_merge --arch movenet --keep_res --resume
 # flip test
