563fc462bbb8d05af9dbe8b52a79b123ccb1e2e5
lee-man <minli.amoy@gmail.com>
2021-07-27 20:31:05 +0800

    update.

--- src/lib/trains/train_factory.py
+++ src/lib/trains/train_factory.py
@@ -6,10 +6,12 @@ from .ctdet import CtdetTrainer
 from .ddd import DddTrainer
 from .exdet import ExdetTrainer
 from .multi_pose import MultiPoseTrainer
+from .single_pose import SinglePoseTrainer
 
 train_factory = {
   'exdet': ExdetTrainer, 
   'ddd': DddTrainer,
   'ctdet': CtdetTrainer,
-  'multi_pose': MultiPoseTrainer, 
+  'multi_pose': MultiPoseTrainer,
+  'single_pose': SinglePoseTrainer
 }
