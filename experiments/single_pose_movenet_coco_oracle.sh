bd8fa0c778ccee965fb1947cd804f8be498ea1d1
lee-man <minli.amoy@gmail.com>
2021-07-28 22:04:25 +0800

    update.

--- experiments/single_pose_movenet_coco_oracle.sh
+++ experiments/single_pose_movenet_coco_oracle.sh
@@ -1,5 +1,5 @@
 cd src
-python main.py single_pose --exp_id coac_movenet_o --dataset active_coco --dataset active_coco --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 150 --lr_step 120 --num_workers 16 --load_model ../models/ctdet_movenet.pth --eval_oracle_offset --eval_oracle_wh
+python main.py single_pose --exp_id coac_movenet_o --dataset active_coco --dataset active_coco --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 200 --lr_step 120,150,180 --num_workers 16 --eval_oracle_offset --eval_oracle_wh --resume #--load_model ../models/ctdet_movenet.pth
 # test
 python test.py single_pose --exp_id coac_movenet_o --dataset active_coco --arch movenet --keep_res --resume
 # flip test
