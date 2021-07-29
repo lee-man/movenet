200c2732337a6f463ac806bbf0ec94bdae39ff5e
lee-man <minli.amoy@gmail.com>
2021-07-29 22:00:31 +0800

    update.

--- experiments/single_pose_movenet_active.sh
+++ experiments/single_pose_movenet_active.sh
@@ -1,5 +1,5 @@
 cd src
-python main.py single_pose --exp_id joga_movenet --dataset active --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 250 --lr_step 120,150,180,200,230  --num_workers 16 --load_model ../models/single_movenet_o.pth --eval_oracle_offset --eval_oracle_wh --not_reg_offset
+python main.py single_pose --exp_id joga_movenet --dataset active --arch movenet --batch_size 24 --master_batch 4 --lr 2.5e-4 --gpus 0,1,2,3 --num_epochs 250 --lr_step 120,150,180,200,230  --num_workers 16 --load_model ../models/single_movenet_o.pth --eval_oracle_offset --eval_oracle_wh 
 
 # test
 python test.py single_pose --exp_id joga_movenet --dataset active --arch movenet --keep_res --resume
